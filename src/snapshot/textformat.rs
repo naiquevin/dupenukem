613c8a216bd40b7053e909607609e0fa020a3a19
Vineet Naik <naikvin@gmail.com>
2023-12-11 19:38:54 +0530

    Implement 'decode' method for the Line enum
    The full implementation for parsing text into a Snapshot is WIP.

--- src/snapshot/textformat.rs
+++ src/snapshot/textformat.rs
@@ -1,6 +1,8 @@
 use super::Snapshot;
+use regex::Regex;
 
 #[allow(dead_code)]
+#[derive(Debug, Eq, PartialEq)]
 enum Line {
     Comment(String),
     MetaData { key: String, val: String },
@@ -22,10 +24,39 @@ impl Line {
         }
     }
 
-
-    fn decode(_s: String) -> Self {
-        // To be implemented
-        Line::Blank
+    fn decode(s: &String) -> Self {
+        let cleaned = s.trim();
+        match &cleaned.chars().next() {
+            Some('#') => {
+                // Either a comment or MetaData
+                if &cleaned[..2] == "#!" {
+                    let re = Regex::new(r"^#!\s*([^:]+):\s*(.+)$").unwrap();
+                    let caps = re.captures(&cleaned).unwrap();
+                    Self::MetaData {
+                        key: caps.get(1).unwrap().as_str().to_owned(),
+                        val: caps.get(2).unwrap().as_str().to_owned(),
+                    }
+                } else {
+                    let re = Regex::new(r"^#\s(.+)$").unwrap();
+                    let caps = re.captures(&cleaned).unwrap();
+                    Self::Comment(caps.get(1).unwrap().as_str().to_owned())
+                }
+            },
+            Some('[') => {
+                let re = Regex::new(r"^\[([^\]]+)\]").unwrap();
+                let caps = re.captures(&cleaned).unwrap();
+                Self::Checksum(caps.get(1).unwrap().as_str().to_owned())
+            },
+            Some(_) => {
+                let re = Regex::new(r"^(keep|symlink|delete)\s(.+)$").unwrap();
+                let caps = re.captures(&cleaned).unwrap();
+                Self::PathInfo {
+                    op: caps.get(1).unwrap().as_str().to_owned(),
+                    path: caps.get(2).unwrap().as_str().to_owned()
+                }
+            }
+            None => Self::Blank
+        }
     }
 }
 
@@ -69,4 +100,48 @@ pub fn render(snap: &Snapshot) -> Vec<String> {
         result.push(line.encode());
     }
     result
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_line_decode_blank() {
+        let x = Line::decode(&"".to_owned());
+        assert_eq!(Line::Blank, x);
+
+        let x = Line::decode(&"  ".to_owned());
+        assert_eq!(Line::Blank, x);
+    }
+
+    #[test]
+    fn test_line_decode_comment() {
+        let x = Line::decode(&"# This is a comment".to_owned());
+        assert_eq!(Line::Comment("This is a comment".to_owned()), x);
+    }
+
+    #[test]
+    fn test_line_decode_metadata() {
+        let x = Line::decode(&"#! Root Directory: /path/to/rootdir".to_owned());
+        assert_eq!(Line::MetaData {
+            key: "Root Directory".to_owned(),
+            val: "/path/to/rootdir".to_owned(),
+        }, x);
+
+        // Without space after colon
+        let x = Line::decode(&"#! Root Directory:/path/to/rootdir".to_owned());
+        assert_eq!(Line::MetaData {
+            key: "Root Directory".to_owned(),
+            val: "/path/to/rootdir".to_owned(),
+        }, x);
+
+        // Without space after exclamation
+        let x = Line::decode(&"#!Root Directory:/path/to/rootdir".to_owned());
+        assert_eq!(Line::MetaData {
+            key: "Root Directory".to_owned(),
+            val: "/path/to/rootdir".to_owned(),
+        }, x);
+    }
 }
